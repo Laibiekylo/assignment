ed2462dade8f5de6b1a4536ca786436c1da2a240
Laibiekylo <klyo129@163.com>
2024-12-16 10:30:46 +0800

    Add files via upload

---
+++ week12/task1.2.java
@@ -0,0 +1,9 @@
+public class c2{
+    public static void main(String[] args){
+        ex1 c1=new ex1();
+        System.out.println(c1.x);
+        System.out.println(c1.y);
+        c1.m1();
+        c1.m2();
+    }
+}
